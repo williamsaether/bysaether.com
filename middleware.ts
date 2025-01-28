7c7743ffcab48e593cd4d3bb6ddc8d4200c29b00
Dobzicle <669810@stud.hvl.no>
2025-01-28 22:30:04 +0100

    fixes to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,33 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|images/|[\\w-]+\\.\\w+).*)",
+  ],
+};
+
 export function middleware(req: NextRequest) {
   const host = req.headers.get('host') || '';
-
   const [subdomain] = host.split('.');
 
+  const url = req.nextUrl.clone();
+
   if (subdomain === 'contact') {
-    const url = req.nextUrl.clone();
-    url.pathname = `/contact`;
-    return NextResponse.rewrite(url);
+    if (url.pathname === '/') {
+      url.pathname = `/contact`;
+      return NextResponse.rewrite(url);
+    } else {
+      url.hostname = 'bysaether.com'
+      return NextResponse.rewrite(url);
+    }
   }
 
   return NextResponse.next();
