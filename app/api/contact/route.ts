028b52330544e02a77f7a5938a260d2cd0163c1b
William Sæther <william.saether03@gmail.com>
2025-11-21 19:44:00 +0100

    fix: add honeypot and prevent automated spam submissions

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -11,41 +11,44 @@ export async function POST(req: NextRequest) {
   const mg = mailgun.client({username: 'api', key: API_KEY, url: 'https://api.eu.mailgun.net'});
 
   const body = await req.json()
-  let {name, email, message} = body
+  let {name, email, message, website} = body
+
+  if (website && website.trim() !== "") {
+    return NextResponse.json({ success: true }); // ignore spam (silently)
+  }
 
   if (!name || !email || !message) {
     return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
   }
+  
+  const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!validEmail) {
+    return NextResponse.json({ success: false, message: "Invalid email address" }, { status: 400});
+  }
 
   const sName = name.replace(/[^a-zæøåA-ZÆØÅ0-9\s'-]/g, '')
-  const sEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) ? email : ''
   const sMessage = message.replace(/[^a-zA-Z0-9\s.,!?'"@#$%&*()-]/g, '')
 
-  if (!email) throw new Error('Invalid email address')
-
   try {
-    mg.messages.create(DOMAIN, {
+    await mg.messages.create(DOMAIN, {
       from: 'Contact Form <contact@mg.bysaether.com>',
       to: [supportEmail],
       subject: 'New Contact Form Submission on BySaether.com',
       text: `
-        Hello,
-        
         You have a new form entry: 
         
         Name: ${sName}
-        Email: ${sEmail}
+        Email: ${email}
         
         Message:
         ${sMessage}
       `,
-      replyTo: [sEmail],
-    })
-    .then(msg => console.log(msg))
+      replyTo: email,
+    });
 
     return NextResponse.json({ success: true, message: 'Thank you! We will reach out to you soon.' }, { status: 200 })
   } catch (err) {
-    console.error('Error sending email', err)
+    console.error('Mailgun error:', err)
     return NextResponse.json(
       { success: false, message: 'An error occurred while sending the email. Please try again later.' },
       { status: 500 }
